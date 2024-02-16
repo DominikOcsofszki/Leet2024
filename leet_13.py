d976144c31d910d453ac85aae5cbfd412e21fcd5
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2024-02-16 15:25:46 +0100

    13. Roman to Integer slow

--- leet_13.py
+++ leet_13.py
@@ -1,4 +1,12 @@
 class Solution:
+    def rules(self, a, b):
+        if(a == 1 and (b == 5 or b == 10)):
+            return b - a, True
+        if(a == 10 and (b == 50 or b == 100)):
+            return b - a, True
+        if(a == 100 and (b == 500 or b == 1000)):
+            return b - a, True
+        return a, False
     def romanToInt(self, s: str) -> int:
         dict = {
             "I": 1,
@@ -10,15 +18,25 @@ class Solution:
             "M": 1000
         }
         result = 0
-        for text in s:
-            txt = dict[text]
-            result += int(txt)
+        skip = False
+        for i in range(len(s)):
+            if skip:
+                skip = False
+                continue
+            nr = int(dict[s[i]])
+            if i == len(s) - 1:
+                result += nr
+                continue
+            nr_next = int(dict[s[i+1]])
+            calc , skip= self.rules(nr, nr_next)
+            result += calc
         return result
-            
+
+
+
 sol = Solution().romanToInt("III")
 print(sol)
 sol2 = Solution().romanToInt("LVIII")
 print(sol2)
-
 sol3 = Solution().romanToInt("MCMXCIV")
 print(sol3)
