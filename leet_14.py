f00b54e9f6245e565301536a961b7e7912922569
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2024-02-16 16:20:17 +0100

    https://leetcode.com/problems/longest-common-prefix/submissions/1177044432/

--- leet_14.py
+++ leet_14.py
@@ -1,12 +1,23 @@
+
+
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-       for x in strs[0]:
-           print(x)
-
-
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+            res = ""
+            strs.sort()
+            maxLen = min(len(strs[0]), len(strs[-1]))
+            for i in range(maxLen):
+                if strs[0][i] is not strs[-1][i]:
+                     return res
+                res += strs[0][i]
+            return res
 
 test1 = ["flower","flow","flight"]
 test2 = ["dog","racecar","car"]
+test3 = ["ab", "a"]
 
-Solution().longestCommonPrefix(strs=test1)
-Solution().longestCommonPrefix(strs=test2)
+res1 = Solution().longestCommonPrefix(strs=test1)
+res2 = Solution().longestCommonPrefix(strs=test2)
+res3 = Solution().longestCommonPrefix(strs=test3)
+print(res1)
+print(res2)
+print(res3)
