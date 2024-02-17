11ae41916aa006bbf7e676d98ae07bc4b42019f0
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2024-02-17 18:08:52 +0100

    leet_27 https://leetcode.com/problems/remove-element/submissions/1178071441/

--- leet_27.py
+++ leet_27.py
@@ -1,6 +1,24 @@
+class Solution1:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        while nums.__contains__(val):
+            nums.remove(val)
+        return len(nums)
+    
+# class Solution:
+#     def removeElement(self, nums: list[int], val: int) -> int:
+#         count = 0
+#         for i in range(len(nums)):
+#             if nums[i] == val:
+#                 nums[i] = 999
+#         return len(nums)-count
 class Solution:
     def removeElement(self, nums: list[int], val: int) -> int:
-        while nums.index[val]:
-            nums.remove(val)
-        print(nums)
-        return nums
+        index = 0
+        for i in range(len(nums)):
+            if not nums[i] == val:
+                nums[index] = nums[i]
+                index +=1
+        return index
+nums =[0,1,2,2,3,0,4,2]
+res =Solution().removeElement(nums, 2)
+print(res)
