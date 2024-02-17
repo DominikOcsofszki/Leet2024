d01c65cfccff687d231a8c103a383f013b4199e0
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2024-02-17 19:14:56 +0100

    leet_29 https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/submissions/1178131657/

---
+++ leet_29.py
@@ -0,0 +1,12 @@
+class Solution1:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        for i in range(len(haystack) - len(needle) +1):
+            if haystack[i : i+len(needle)] == needle:
+                return i
+
+        return -1
+print(Solution().strStr("abcaa","aa"))
