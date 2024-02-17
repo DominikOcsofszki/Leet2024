7fbdea4a7cf4bc64234a7104e05ec5a3d0dad5ea
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2024-02-17 17:46:23 +0100

    leet_21 https://leetcode.com/problems/merge-two-sorted-lists/submissions/1178047460/

---
+++ leet_27.py
@@ -0,0 +1,6 @@
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        while nums.index[val]:
+            nums.remove(val)
+        print(nums)
+        return nums
